202ff35930150e6d39c8565555cff77b4251c836
sjw242 <sjw242@hanmail.net>
2023-01-02 18:43:58 +0900

    day1-1

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const ctx = canvas.getContext("2d");    //소문자 d
 canvas.width=800;
 canvas.height=800;
 
-ctx.rect(50, 50, 100, 100);
+ctx.rect(50, 50, 100, 100);     // 가로위치, 세로위치, 가로길이, 세로길이
 ctx.rect(150, 150, 100, 100);
 ctx.fill();     // fill 위까지 묶여있음
 
+ctx.beginPath();        // 이전 경로와 단절
 ctx.rect(250, 250, 100, 100);
+ctx.rect(350, 350, 100, 100);
 ctx.fillStyle = "red";
-setTimeout(() => {ctx.fill()}, 5000);   // 5초후에 fill 적용
-
-
+ctx.fill();
