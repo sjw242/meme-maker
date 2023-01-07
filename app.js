e772363a4a063d361a213ec107de6c19f3155872
sjw242 <sjw242@hanmail.net>
2023-01-07 16:32:37 +0900

    day2-1

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const ctx = canvas.getContext("2d");    //소문자 d
 canvas.width=800;
 canvas.height=800;
 
-ctx.rect(50, 50, 100, 100);     // 가로위치, 세로위치, 가로길이, 세로길이
-ctx.rect(150, 150, 100, 100);
-ctx.fill();     // fill 위까지 묶여있음
-
-ctx.beginPath();        // 이전 경로와 단절
-ctx.rect(250, 250, 100, 100);
-ctx.rect(350, 350, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill();
+ctx.moveTo(50, 50);   // 브러쉬 위치 이동
+ctx.lineTo(150, 50);    // 직전 브러쉬 위치부터 해당 위치까지 선긋기
+ctx.lineTo(150, 150);   
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.stroke();       // 선 칠하기
