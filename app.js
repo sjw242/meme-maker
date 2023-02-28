bdf8499b3a059cd1b7746772378c05f9a289291c
sjw242 <93955388+sjw242@users.noreply.github.com>
2023-02-28 23:50:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const ctx = canvas.getContext("2d");    //소문자 d
 canvas.width=800;
 canvas.height=800;
 
-ctx.fillRect(210, 200, 15, 100);
-ctx.fillRect(350, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
+ctx.lineWidth = 2;
+ctx.moveTo(0, 0);       // 처음위치 지정
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);    // 가로세로위치, 너비, 시작지점, 끝지점
-ctx.fill();
+// flatuicolors.com 색깔 가져오기
+const colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e"];
 
-ctx.beginPath();    // 지점 새로 시작하는건 필수
-ctx.arc(260+10, 100, 8, Math.PI, 2 * Math.PI);      // 원 모양 유지하려면 시작지점도 원 수식
-ctx.arc(220+10, 100, 8, Math.PI, 2 * Math.PI);
-ctx.fillStyle = "red";
-ctx.fill();
+function onClick(event){
+    // ctx.moveTo(0, 0);        처음위치 고정
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("click", onClick);
+
